f67f7a0d143430c1a9015c3f9a9222eff2cefd62
RhiannaJS <rhianna.latshaw@gmail.com>
2021-10-12 18:07:37 -0400

    environment file changes

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,8 +4,8 @@ switch (window.location.hostname) {
     case "localhost" || "127.0.0.1" :
         APIURL = "http://localhost:4000";
         break;
-    case "rjs-concert-app.herokuapp.com" :
-        APIURL = "https://rjs-my-concert-experience.herokuapp.com"
+    case "rjs-my-concert-experience.herokuapp.com" :
+        APIURL = "https://rjs-concert-app.herokuapp.com"
 }
 
 export default APIURL;
